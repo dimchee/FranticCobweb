127f329396e9c20b2f00dce28241402c8aead75c
dimchee <dimchee90@gmail.com>
2025-02-25 22:03:00 +0100

    Add firing with mouse

--- net.gd
+++ net.gd
@@ -1,24 +1,31 @@
 extends Node2D
 
+var speed = 1
+
 class Nit:
 	var start: Vector2 
 	var end: Vector2
-	func _init(start: Vector2, end: Vector2) -> void:
+	var progress: float
+	func _init(start: Vector2, end: Vector2, progress: float = 0.0) -> void:
 		self.start = start
 		self.end = end
-		
+		self.progress = progress
 
-var netholder = [ Nit.new(Vector2(-300, 300), Vector2(300, 300))
-				, Nit.new(Vector2(0, 0), Vector2(-300, 300)) 
-				]
+var threads = [ 
+	Nit.new(Vector2(-300, 300), Vector2(300, 300), 1.0), 
+	Nit.new(Vector2(0, 0), Vector2(-300, 300), 1.0) 
+]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _draw() -> void:
-	for t in netholder:
-		draw_line(t.start, t.end, Color.WHITE, 2)
+	for t in threads:
+		draw_line(t.start, lerp(t.start, t.end, t.progress), Color.WHITE, 2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	for t in threads:
+		if t.progress + delta*0.1 < 1.0:
+			t.progress = t.progress + delta*speed
+			self.queue_redraw()
--- spider.gd
+++ spider.gd
@@ -1,11 +1,13 @@
 extends Sprite2D
 
-
+var Net
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	self.Net = get_node("/root/Main/Net")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var vp = get_viewport()
+	if Input.is_action_pressed("Fire"):
+		Net.threads.append(Net.Nit.new(self.position, vp.get_mouse_position() - vp.get_visible_rect().size / 2))
+		Net.queue_redraw()
